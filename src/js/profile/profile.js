f59e60174f66d7033898c0161831c0814a0e0e53
Gabriel Zouad <z20gab@gmail.com>
2023-12-13 13:29:23 +0100

    changed color for wcag and added new image with webp format

--- src/js/profile/profile.js
+++ src/js/profile/profile.js
@@ -94,7 +94,7 @@ function displayUserListings(listings) {
       <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <article class="flex max-w-xl flex-col items-start justify-between">
         <div class="group relative">
-          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-blue-700">
             <a href="listingpage.html?id=${listing.id}">
               ${listing.title}
             </a>
@@ -117,7 +117,7 @@ function displayUserBids(bids) {
       <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <article class="flex max-w-xl flex-col items-start justify-between">
           <div class="group relative">
-            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <h3 class="mt-3 text-lg font-semibold leading-6 text-blue-700">
               <a href="listingpage.html?id=${bid.listing.id}">
                 ${bid.listing.title}
               </a>
